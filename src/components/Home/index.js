927d3a650a9b283c07d55b82df08279ae76bb3b2
Farhanalisayed <151043508+Farhanalisayed@users.noreply.github.com>
2024-01-02 15:16:02 +0530

    Update index.js

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,49 +1,25 @@
-import {Component} from 'react'
-import {Link} from 'react-router-dom'
-import {AiFillHome} from 'react-icons/ai'
-import {BsInfoCircleFill} from 'react-icons/bs'
 import Header from '../Header'
 import './index.css'
 
-class Home extends Component {
-  state = {isClicked: false}
-  onClicked = () => {
-    this.setState(prevState => ({isClicked: !prevState.isClicked}))
-  }
-
-  renderHome = () => {
-    const homeUrl =
-      screen.width < 768
-        ? 'https://assets.ccbp.in/frontend/react-js/home-sm-img.png'
-        : 'https://assets.ccbp.in/frontend/react-js/home-lg-img.png'
-    return (
-      <div className="home-cont">
-        <img className="image" src={homeUrl} alt="home" />
+const Home = () => (
+    <div className="the-cont">
+      <Header />
+      <div className="home-cont-md">
+        <img
+          className="image"
+          src="https://assets.ccbp.in/frontend/react-js/home-lg-img.png"
+          alt="home"
+        />
         <h1>Home</h1>
       </div>
-    )
-  }
-  renderOptions = () => (
-    <div className="options">
-      <Link to="/">
-        <AiFillHome className="route-icon" />
-      </Link>
-
-      <Link to="/about">
-        <BsInfoCircleFill className="route-icon" />
-      </Link>
+      <div className="home-cont-sm">
+        <img
+          className="image"
+          src="https://assets.ccbp.in/frontend/react-js/home-sm-img.png"
+          alt="home"
+        />
+        <h1>Home</h1>
+      </div>
     </div>
   )
-
-  render() {
-    const {isClicked} = this.props
-
-    return (
-      <div className="the-cont">
-        <Header onClicked={this.onClicked} />
-        {isClicked ? this.renderOptions() : this.renderHome()}
-      </div>
-    )
-  }
-}
 export default Home
