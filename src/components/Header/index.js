324eb2fe0c9cccc151c96ccf3efc4370a9fbb75a
Farhanalisayed <151043508+Farhanalisayed@users.noreply.github.com>
2024-01-02 15:12:35 +0530

    Update index.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,58 +1,71 @@
-import {Component} from 'react'
+import Popup from 'reactjs-popup'
 import {Link} from 'react-router-dom'
 import {GiHamburgerMenu} from 'react-icons/gi'
 import {IoMdClose} from 'react-icons/io'
+import {AiFillHome} from 'react-icons/ai'
+import {BsInfoCircleFill} from 'react-icons/bs'
 
-import Popup from 'reactjs-popup'
-import 'reactjs-popup/dist/index.css'
 import './index.css'
 
-class Header extends Component {
-  onClickedHam = () => {
-    const {onClicked} = this.props
-    onClicked()
-  }
+const Header = () => {
+  const overlayStyles = {
+  backgroundColor: '#ffff',
+  color:'black',
+  width:'100%'
+ }
 
-  render() {
-    const overlayStyles = {
-      backgroundColor: '#ffff',color:'black'
-    }
-    return (
-      <div className="the-cont">
-      <div>
-        <Link to="/">
-          <img
-            className="icon"
-            src="https://assets.ccbp.in/frontend/react-js/hamburger-menu-website-logo.png"
-            alt="website logo"
-          />
-        </Link>
-      </div>
-
-      <div>
-        <Popup
-          trigger={
-            <GiHamburgerMenu
-              className="ham-icon"
-              data-testid="hamburgerIconButton"
-              onClick={this.onClickedHam}
-            />}
-          overlayStyle={overlayStyles}
-          position="none"
-        >
-          {close => (
-            <>
-              <IoMdClose
-                className="ham-icon"
-                onClick={() => close()}
-                data-testid="closeButton"
-              />
-            </>
-          )}
-        </Popup>
-        </div>
-      </div>
-    )
-  }
+  return(
+  <div className="the-cont">
+    <div>
+      <Link to="/">
+        <img
+          className="icon"
+          src="https://assets.ccbp.in/frontend/react-js/hamburger-menu-website-logo.png"
+          alt="website logo"
+        />
+      </Link>
+    </div>
+    <div>
+      <Popup
+        modal
+        trigger={
+          <button data-testid="hamburgerIconButton">
+            <GiHamburgerMenu className="ham-icon" />
+          </button>
+        }
+        overlayStyle={overlayStyles}
+        className="popup-content"
+      >
+        {close => (
+          <>
+            <button
+              data-testid="closeButton"
+              className="closeBut"
+              onClick={() => {
+                close()
+              }}
+            >
+              <IoMdClose className="ham-icon" />
+            </button>
+            <ul>
+              <li>
+                <Link to="/">
+                  <AiFillHome className="route-icon" />
+                  <p>Home</p>
+                </Link>
+              </li>
+              <li>
+                <Link to="/about">
+                  <BsInfoCircleFill className="route-icon" />
+                  <p>About</p>
+                </Link>
+              </li>
+            </ul>
+          </>
+        )}
+      </Popup>
+    </div>
+  </div>
+)
 }
 export default Header
